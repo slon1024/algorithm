adb5f3ea07e66215943fa7f50653d08670fb75d0
Vladimir Alekseichenko <vova@vova.me>
2012-07-18 00:29:53 +0200

    optimize merge

--- python/src/sort/merge_sort.py
+++ python/src/sort/merge_sort.py
@@ -4,7 +4,7 @@ def merge_sort(in_array):
     if length < 2:
         return in_array
 
-    middle = length / 2
+    middle = length >> 1
     left   = merge_sort( in_array[:middle] )
     right  = merge_sort( in_array[middle:])
     return merge( left, right )
@@ -13,8 +13,9 @@ def merge_sort(in_array):
 def merge(left, right):
     result = []
     i,j = 0,0
-
-    while i < len(left) and j < len(right):
+    len_i, len_j = len(left), len(right)
+    
+    while i < len_i and j < len_j:
         if left[i] < right[j]:
             result += [ left[i] ]
             i += 1
@@ -22,8 +23,5 @@ def merge(left, right):
             result += [ right[j] ]
             j += 1
 
-    result += left[i:]
-    result += right[j:]
-    return result
-
+    return result + left[i:] + right[j:]
 
