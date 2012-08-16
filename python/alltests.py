c7ecfc5974780749ccee677640ccbd9538c02cad
Vladimir Alekseichenko <vova@vova.me>
2012-08-16 23:36:22 +0200

    alltest.py

--- python/alltests.py
+++ python/alltests.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     dir_tests = 'tests'
     dirs=[ item for item in os.listdir('./tests')  if os.path.isdir( dir_tests + '/' + item) ]
     
-    
     test_file_strings= [ y for x in dirs for y in glob.glob(dir_tests + '/' + x + '/*_test.py') ]
     module_strings = [  file_string[0:len(file_string) - 3].replace('/', '.') for file_string in test_file_strings]
 
