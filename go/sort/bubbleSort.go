b80e71534cbcfc58a7c62765399f751243ccd744
Vladimir Alekseichenko <vova@vova.me>
2013-11-04 06:24:28 +0100

    bubleSort

--- go/sort/bubbleSort.go
+++ go/sort/bubbleSort.go
@@ -1,14 +1,14 @@
 package sort
 
 func BubleSort(inArray []int) []int {
-	length := len(inArray)
-	for i := 0; i < length-1; i++ {
-		for j := i + 1; j < length; j++ {
-			if inArray[j] < inArray[i] {
-				inArray[i], inArray[j] = inArray[j], inArray[i]
-			}
-		}
-	}
+    length := len(inArray)
+    for i := 0; i < length-1; i++ {
+	    for j := i + 1; j < length; j++ {
+            if inArray[j] < inArray[i] {
+                inArray[i], inArray[j] = inArray[j], inArray[i]
+            }
+        }
+    }
 
-	return inArray
+    return inArray
 }
