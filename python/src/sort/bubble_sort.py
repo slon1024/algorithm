cd42b5ffcb5fa18865f4761100e3f6f1b6e50882
Vladimir Alekseichenko <vova@vova.me>
2013-11-04 06:31:50 +0100

    [py] bubleSort

--- python/src/sort/bubble_sort.py
+++ python/src/sort/bubble_sort.py
@@ -1,12 +1,8 @@
 
 def bubble_sort(in_array):
     length = len(in_array)
-    for i in range(length):
+    for i in range(length-1):
         for j in range(i+1, length):
             if in_array[j] < in_array[i]:
                 in_array[i], in_array[j] = in_array[j], in_array[i]
     return in_array
-
-
-    
-
