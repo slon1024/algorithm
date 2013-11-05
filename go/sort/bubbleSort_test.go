ddffbb176d47463dd57b1df9a986ebbefc4fd710
Vladimir Alekseichenko <vova@vova.me>
2013-11-05 16:37:51 +0100

    [go] refactoring bubleSort

--- go/sort/bubbleSort_test.go
+++ go/sort/bubbleSort_test.go
@@ -1,38 +1,16 @@
 package sort
+import "testing"
 
-import (
-    "math/rand"
-    "sort"
-    "testing"
-)
-
-func TestEmptyInput(t *testing.T) {
+func TestBubleSortEmptyInput(t *testing.T) {
     values := []int{}
-
     baseTest(values, t)
 }
 
-func TestBigData(t *testing.T) {
-    values := make([]int, 1000)
-    for i := 0; i < len(values); i++ {
-        values[i] = rand.Intn(100000)
-    }
-
-    baseTest(values, t)
+func TestBubleSortBigData(t *testing.T) {
+    baseTest(genBigArray(), t)
 }
 
-func TestNegativeValues(t *testing.T) {
+func TestBubleSortNegativeValues(t *testing.T) {
     values := []int{-3, -5, -1, -15}
-
     baseTest(values, t)
-}
-
-func baseTest(values []int, t *testing.T) {
-    actualValues := BubleSort(values)
-    areSorted := sort.IntsAreSorted(actualValues)
-
-    if areSorted == false {
-        sort.Ints(values)
-        t.Errorf("There're not sorted. Actual values: %v, expected values: %v", actualValues, values)
-    }
 }
